c071bb9b89bed8972d496bf934ae41bbdd1639a0
Cuma YILDIZ <142337560+cumaayildizz@users.noreply.github.com>
2023-09-24 00:11:08 +0300

    PalindromikKelimeBulma
    Palindromik Kelime Bulma oyunu
    palindromik kelime duz ve ters okunusu ayni olan
    asa tersi asa
    kavak tersi kavak

---
+++ PalindromikKelimeBulma.java
@@ -0,0 +1,48 @@
+package metotsVeDizi;
+
+import java.util.Scanner;
+
+public class PalindromikKelimeBulma {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String userWord ;
+
+        System.out.println();
+        System.out.println("*******PALINDROMIK KELIME BULMA PROGRAMI*******");
+        System.out.println();
+
+        System.out.print("Kelime girin : ");
+        userWord = input.nextLine();
+        System.out.println();
+
+        String reverseWord = "";
+        for (int i = userWord.length() - 1; i >= 0; i--) {
+            reverseWord += userWord.charAt(i);
+        }
+
+        if ( userWord.equals(reverseWord)){
+            System.out.println( "* " + userWord + " *" + "  palindoromik bir kelimedir.");
+        }
+        else  {
+            System.out.println( "* " + userWord + " *" + "  palindoromik DEGILDIR.");
+        }
+
+
+
+       /*
+          int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        */
+
+
+
+
+    }
+}
